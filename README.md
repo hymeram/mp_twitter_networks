853b4243a94870de4c67e9d564b631cac29ee59f
hymeram <hymeram@gmail.com>
2022-04-17 15:34:41 +0100

    Add latent space model code and plot

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # MPs' Twitter Networks 
-This project used Twitter's API to collect, visualize, and analyse data on how politicians interact on social media. 
+Twitter's API can be used to collect data on how politicians interact with each other and other users on the platform. This data an be used to study their behavior and political preferences.
 
 The code contained here is the cleaned up version of some of the code I used for my master's dissertation. This dissertation looked at how ideology and factional membership influence how MPs interact with each other on Twitter.
 
 ### MPs' Twitter Like Network 
-Here's the output of the script - the thickness of the edges indicate the number of times the MPs have liked each others tweets. Colour indicates party membership. Location of the nodes calculated from Fruchterman Reingold layout algorithm.
+The python script 'tweet_like_visualisation' collects the data using Twitters API and visualizes the network. the thickness of the edges indicate the number of times the MPs have liked each others tweets and colour indicates party membership.
 
 ![alt text](https://github.com/hymeram/mp_twitter_networks/blob/main/like_network.png)
+
+### Analysing the Network 
+The dataset 'like_matrix.csv' shows the likes each MP gave each other over a period in 2020 and 2021. This data can then be used to analyse their behaviour. To give a simple example the R script 'lsm_ideology_estimation' condenses the data into two dimensional latent space where the positions of the MPs can be used to infer their ideological distance from one another.
+
+![alt text](https://github.com/hymeram/mp_twitter_networks/blob/main/MP_latent_space_plot.png) 
+
+The above shows MPs positions in latent space. MPs tend to interact more with other members of their party and are hence close in latent space. MPs from parties who are ideologically similar are also closely positioned in latent space.DUP MPs for example are positioned close to Conservative MPs.
---
+++ scripts/lsm_ideology_estimatation.r
@@ -0,0 +1,63 @@
+#### Set-up ####
+library("latentnet") 
+library('network')
+library('sna')
+library('dplyr')
+library('ggplot2')
+
+setwd("C:/Users/Alex/Documents/Misc/mp_twitter_networks")
+
+#### Data Prep #####
+df <- read.csv('data/like_matrix.csv',encoding="UTF-8")
+
+rownames(df) <- df$X
+df <- subset(df, select = -c(X))
+diag(df) <- 0 # remove self-loops
+
+#### Network ####
+m <- as.matrix(df)
+network <- network(m, directed = FALSE, weighted=TRUE)
+set.edge.value(network,"weight",m)
+
+#### Latent Space Model #### 
+lsm_fit <- ergmm(network ~ rsociality
+                   + euclidean(d=2),
+                   family="Poisson",
+                   verbose=TRUE, 
+                   response = "weight")
+summary(lsm_fit)
+
+#### Plotting MP ideology ####
+
+#getting the data ready
+mp_details <- read.csv('data/MP_Twitter_Accounts_2021.csv',encoding="UTF-8")
+
+ideology <- lsm_fit$mcmc.pmode$Z %>% #extracting latent positions from model
+            as.data.frame()
+ideology$mp <- rownames(df) 
+ideology <- merge(ideology, mp_details, by.x='mp', by.y='display_name') 
+ideology <- ideology %>% 
+  mutate(party2 = 
+           case_when(party == "Conservative" ~ "Conservative", 
+                     party == "Labour" ~ "Labour",
+                     party == "Liberal Democrat" ~ "Liberal Democrat", 
+                     party == "Scottish National Party" ~ "SNP",
+                     party == "Sinn Féin " ~ "Sinn Féin", 
+                     party == "Democratic Unionist Party" ~"DUP", 
+                     TRUE ~ "Other"))
+
+#plotting
+ggplot(data=ideology, aes(V1,V2,colour=party2)) + 
+  geom_point(size=1) + 
+  theme_minimal() + 
+  theme(axis.title = element_blank()) +
+  scale_colour_manual("Party",
+                      values=c("blue","red","orange","gold",
+                               "lightgreen","black","grey"), 
+                      breaks = c("Conservative", "Labour", "Liberal Democrat", 
+                                 "SNP","Sinn Féin","DUP","Other"))
+   
+  
+ggsave("MP_latent_space_plot.png",
+       width=10, height=6, units="in",
+       bg="white")  
